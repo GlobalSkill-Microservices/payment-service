be87d23bd246e6ab30dcec182e571236ea0c9e04
QuanDong <quanonepunch@gmail.com>
2025-10-21 08:30:57 +0700

    add request json webhook

--- payment-service/src/main/java/com/globalskills/payment_service/Payment/Dto/WebhookRequest.java
+++ payment-service/src/main/java/com/globalskills/payment_service/Payment/Dto/WebhookRequest.java
@@ -1,19 +1,57 @@
 package com.globalskills.payment_service.Payment.Dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class WebhookRequest {
 
-    String code;
+    @JsonProperty("gateway")
+    private String gateway;
+
+    @JsonProperty("transactionDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime transactionDate;
+
+    @JsonProperty("accountNumber")
+    private String accountNumber;
+
+    @JsonProperty("subAccount")
+    private String subAccount;
+
+    @JsonProperty("code")
+    private String code;
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("transferType")
+    private String transferType;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("transferAmount")
+    private long transferAmount;
+
+    @JsonProperty("referenceCode")
+    private String referenceCode;
+
+    @JsonProperty("accumulated")
+    private long accumulated;
+
+    @JsonProperty("id")
+    private long id;
 
 }
